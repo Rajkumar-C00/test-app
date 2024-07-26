fd369bcd0f32cef9e3ad0683f3f040f39f2c6905
rc185401_ncrvoyix <rajkumar.c@ncrvoyix.com>
2024-07-26 12:33:50 +0530

    test commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            string version = "1.0";
+            version = ConfigurationManager.AppSettings["webpages:Version"];
+
+            ViewBag.Message = $"Your Version {version} application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            string version = "1.0";
+            version = ConfigurationManager.AppSettings["webpages:Version"];
+
+            ViewBag.Message = $"Your Version {version} contact page.";
+
+            return View();
+        }
+    }
+}
